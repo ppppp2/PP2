4746a995437d4611d782d1d7a334406135aa7b0d
Matias <Matias@192.168.0.12>
2018-03-17 22:04:55 -0300

    tt

---
+++ src/main/java/Ca.java
@@ -0,0 +1,4 @@
+
+public class Ca {
+	static int s(int a, int b) {return a+b;}
+}
---
+++ src/test/java/caTest.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class caTest {
+
+	@Test
+	public void test() {
+		assertEquals(Ca.s(1, 2),3);
+		
+	}
+
+}
